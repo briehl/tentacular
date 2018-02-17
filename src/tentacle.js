6357f43e862d133b62d16da0f1fe47f076ae315d
briehl <briehl@gmail.com>
2018-02-17 15:28:33 -0800

    initial commit, more or less

---
+++ src/tentacle.js
@@ -0,0 +1,11 @@
+/**
+ * Makes a single tentacle.
+ * A tentacle is roughly defined as starting with a thick circle, then circles with varying
+ * radii, tangent to each other at some angle, until some minimum size is met.
+ */
+
+import * as d3 from 'd3';
+
+export function tentacle () {
+    console.warn('i am a tentacle.');
+}
